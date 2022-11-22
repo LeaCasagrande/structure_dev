ca6d69078bbae2330444500a96cf0b6e1c68b59d
Léa Casagrande <lea.casagrande27@gmail.com>
2022-11-22 17:02:26 +0100

    22.11.2022

--- 5_Exercices_En_Cours_Formation/JavaScript/exoJavaScript.js
+++ 5_Exercices_En_Cours_Formation/JavaScript/exoJavaScript.js
@@ -94,7 +94,7 @@ function calcul() {
     // modulo(n1,n2);
 }
 
-calcul();
+// calcul();
 
 // POO
 
@@ -129,3 +129,126 @@ console.log(z1.getReel());
 
 let z2 = new Complexe(26,4);
 console.log(z2.getReel());
+
+// Exo Rectangle
+// Class Rectangle
+// attributs: longueur, largeur
+// getters, setters, toString, getPerimetre, getSurface, test de la classe
+
+class Rectangle {
+    constructor(longueur, largeur){
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+    getLongueur(){
+        return this.longueur;
+    }
+    getLargeur(){
+        return this.largeur;
+    }
+    setLongueur(longueur){
+        this.longueur = longueur;
+    }
+    setLargeur(largeur){
+        this.largeur = largeur;
+    }
+    toString(){
+        return ("Longueur :"+this.longueur+"\n"+"Largeur"+this.largeur);
+    }
+    getPerimetre(){
+        return 2*(this.longueur+this.largeur);
+    }
+    getSurface(){
+        return this.longueur*this.largeur;
+    }
+}
+
+let rect1 = new Rectangle(10,4);
+console.log(rect1.getLargeur());
+
+// Valeurs primitives
+
+// String slide 62
+function testString(){
+    let ch1 = prompt("Veuillez entrer une chaîne de caractères");
+    let ch2 = prompt("Veuillez entrer une autre chaîne de caractères");
+    return ch1.includes(ch2) || ch2.includes(ch1);
+}
+
+// testString();
+
+// String, Number, Math slide 71
+function alea(){
+
+    // // Générer un nb décimal entre 100 et 1000
+    // let nbAlea = (Math.random()*1000);
+    // console.log(nbAlea);
+    // // Arrondir ce nb à l'entier le plus proche
+    // nbAlea = Math.round(nbAlea);
+    // console.log(nbAlea);
+    // // Diviser ce nb pour obtenir un nb à une décimale
+    // nbAlea = nbAlea/10;
+    // console.log(nbAlea);
+    // // Négatif ou positif
+    // nbAlea = nbAlea * (Math.round(Math.random()) ? 1 : -1)
+    // console.log("nbAlea : "+nbAlea)
+
+    // Version condensée
+    let nbAlea2 = (Math.round(Math.random()*1000)/10) * (Math.round(Math.random()) ? 1 : -1);
+    alert("nbAlea2 : "+nbAlea2);
+
+    let nbUser;
+    do{
+        nbUser = parseFloat(prompt("Veuillez entrer un nombre."));
+        if(nbUser > nbAlea2){
+            alert("Le nombre à trouver est plus petit.");
+        }else if(nbUser < nbAlea2){
+            alert("Le nombre à trouver est plus grand.");
+        }else{
+            alert("Bravo !")
+        }
+    }while (nbUser != nbAlea2);
+}
+
+//alea();
+
+// Array slide 80
+let semaine = ['lun','mra','mer','jeu','ven','sam']
+semaine.pop();
+console.log(semaine);
+semaine.push('dim');
+semaine[1]='mar';
+document.write(semaine.length);
+console.log(semaine[2]);
+
+// Array, Date slide 90
+function tirageAuSort(){
+    let list = [];
+    let nom;
+    let boucle = true;
+
+    let date = new Date();
+    let dateLocale = date.toLocaleString('fr-FR')
+
+    // while(boucle){
+    //     nom = prompt("Veuillez saisir un nom.");
+    //     if(nom != 0){
+    //         list.push(nom);
+    //     }else{
+    //         boucle = false;
+    //     }
+    // }
+
+    while(nom != 0){
+        nom = prompt("Veuillez saisir un nom.");
+        list.push(nom);
+    }
+
+    list.pop();
+
+    alert(list);
+    alert("Le tirage au sort du "+dateLocale+" a designé comme grand gagnant : "+list[Math.trunc(Math.random()*list.length)]);
+
+}
+
+tirageAuSort();
