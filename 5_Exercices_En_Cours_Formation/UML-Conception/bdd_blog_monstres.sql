fdb6af5acedfde9b431c6cae97d2fae7f7d295b8
LeaCasagrande <lea.casagrande27@gmail.com>
2022-12-21 10:10:59 +0100

    21.12.2022

---
+++ 5_Exercices_En_Cours_Formation/UML-Conception/bdd_blog_monstres.sql
@@ -0,0 +1,112 @@
+create database blog_monstre;
+use blog_monstre;
+
+CREATE TABLE utilisateur(
+id_utilisateur INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+pseudo_utilisateur VARCHAR(50) NOT NULL,
+mail_utilisateur VARCHAR(50) NOT NULL,
+password_utilisateur VARCHAR(100) NOT NULL,
+id_roles INT NOT NULL,
+id_image INT NOT NULL
+)engine=InnoDB;
+
+CREATE TABLE image(
+id_image INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nom_image VARCHAR(100) NOT NULL,
+url_image VARCHAR(100) NOT NULL
+)engine=InnoDB;
+
+CREATE TABLE roles(
+id_roles INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nom_roles VARCHAR(100) NOT NULL
+)engine=InnoDB;
+
+CREATE TABLE article(
+id_article INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+titre_article VARCHAR(50) NOT NULL,
+contenu_article VARCHAR(50) NOT NULL,
+date_article datetime NOT NULL,
+id_utilisateur INT NOT NULL
+)engine=InnoDB;
+
+CREATE TABLE categorie(
+id_categorie INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nom_categorie VARCHAR(50) NOT NULL
+)engine=InnoDB;
+
+CREATE TABLE commentaire(
+id_commentaire INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+date_commentaire datetime NOT NULL,
+contenu_commentaire TEXT NOT NULL
+)engine=InnoDB;
+
+CREATE TABLE imager(
+id_article INT NOT NULL,
+id_image int NOT NULL,
+PRIMARY KEY(id_article, id_image)
+)engine=InnoDB;
+
+CREATE TABLE ecrire(
+id_commentaire INT NOT NULL,
+id_utilisateur INT NOT NULL,
+PRIMARY KEY(id_commentaire, id_utilisateur)
+)engine=InnoDB;
+
+CREATE TABLE associer(
+id_categorie INT NOT NULL,
+id_article INT NOT NULL,
+PRIMARY KEY(id_categorie, id_article)
+)engine=InnoDB;
+
+ALTER TABLE utilisateur
+ADD CONSTRAINT fk_posseder_image
+FOREIGN KEY(id_image)
+REFERENCES image(id_image);
+
+ALTER TABLE utilisateur
+ADD CONSTRAINT fk_attribuer_roles
+FOREIGN KEY(id_roles)
+REFERENCES roles(id_roles);
+
+ALTER TABLE article
+ADD CONSTRAINT fk_rediger_utilisateur
+FOREIGN KEY(id_utilisateur)
+REFERENCES utilisateur(id_utilisateur);
+
+ALTER TABLE imager
+ADD CONSTRAINT fk_imager_image
+FOREIGN KEY(id_image)
+REFERENCES image(id_image);
+
+ALTER TABLE imager
+ADD CONSTRAINT fk_imager_article
+FOREIGN KEY(id_article)
+REFERENCES article(id_article);
+
+ALTER TABLE ecrire
+ADD CONSTRAINT fk_ecrire_utilisateur
+FOREIGN KEY(id_utilisateur)
+REFERENCES utilisateur(id_utilisateur);
+
+ALTER TABLE ecrire
+ADD CONSTRAINT fk_ecrire_commentaire
+FOREIGN KEY(id_commentaire)
+REFERENCES commentaire(id_commentaire);
+
+insert into roles (id_roles, nom_roles) values
+(1,'admin'),
+(2, 'utilisateur');
+
+insert into image (id_image, nom_image, url_image) values
+(1, 'image_4', 'https://lagendageek.com/wp-content/uploads/2021/08/boblennon.jpg');
+
+insert into utilisateur (id_utilisateur, pseudo_utilisateur, mail_utilisateur, password_utilisateur, id_roles, id_image) values
+(1, 'bobbidou', 'bobie@gmail.com', 'azerty77', 2, 1)
+(2, 'Micael', 'aaaaaaaaaaaaaa@gmail.com', 'azerty77', 1, 1);
+
+update utilisateur 
+set password_utilisateur = MD5(password_utilisateur)
+where pseudo_utilisateur = 'bobbidou';
+
+delete from utilisateur 
+where pseudo_utilisateur = 'Micael';
--- 5_Exercices_En_Cours_Formation/UML-Conception/bdd_caisse.sql
+++ 5_Exercices_En_Cours_Formation/UML-Conception/bdd_caisse.sql
@@ -48,3 +48,68 @@ ALTER TABLE ajouter
 ADD constraint fk_ajouter_produit
 FOREIGN KEY(id_produit)
 REFERENCES produit(id_produit);
+
+-- Exercice requête de MAJ insert
+insert into vendeur(nom_vendeur, prenom_vendeur) values 
+('Lambert', 'Valentin'),
+('Crépin', 'Marie'),
+('Gribelin', 'Jade'),
+('Chapelle', 'Yves'),
+('Chauveau', 'Bruno');
+
+insert into produit(nom_produit, prix_produit, description_produit) values
+('beurre', 2.09, 'Plaquette de beurre 250g'),
+('potimarron', 2.49, 'Potimarron bio pièce'),
+('confiture', 1.89, 'Pot de confiture fraise 370g'),
+('spaghetti', 0.92, 'Paquet de spaghetti'),
+('pesto', 1.75, 'Pot de pesto vert');
+
+insert into ticket(date_ticket, id_vendeur) values
+('2022-12-20 10:30:05', 1),
+('2022-12-20 10:30:05', 2),
+('2022-12-20 10:30:05', 3),
+('2022-12-20 10:30:05', 4);
+
+insert into ajouter(id_produit, id_ticket, quantite) values
+(1, 1, 2),
+(2, 1, 2),
+(3, 1, 4),
+(4, 1, 2),
+(5, 1, 3),
+
+(1, 2, 1),
+(2, 2, 1),
+(3, 2, 4),
+(4, 2, 8),
+(5, 2, 4),
+
+(1, 3, 2),
+(2, 3, 5),
+(3, 3, 4),
+(4, 3, 2),
+(5, 3, 3),
+
+(1, 4, 2),
+(2, 4, 2),
+(3, 4, 4),
+(4, 4, 7),
+(5, 4, 3);
+
+-- Exercice requête de MAJ update et delete
+-- mettre à jour les tickets 1 et 2 et remplacer le vendeur par le vendeur qui à pour id_vendeur =4
+update ticket set id_vendeur = 4 where id_ticket IN (1, 2);
+
+-- ajouter 2 vendeurs et supprimez les (1 seule requête pour supprimer)
+insert into vendeur(nom_vendeur, prenom_vendeur) values 
+('Laffitte', 'Denis'),
+('Beauchamp', 'Mathilde');
+delete from vendeur where nom_vendeur IN ('Laffitte','Beauchamp');
+
+-- remplacer le prenom_utilisateur de tous les vendeurs dont le nom_utilisateur commence par 'a' par 'test'
+update vendeur set prenom_vendeur='test' where nom_vendeur LIKE 'A%';
+
+-- mettre en majuscule le nom de chaque vendeur
+update vendeur set nom_vendeur = UPPER(nom_vendeur);
+
+-- ajouter 1 € à tous les produits
+update produit set prix_produit = prix_produit + 1;
